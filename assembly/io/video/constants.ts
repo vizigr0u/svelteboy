861d72a03ec1028f8fa068784712307168e3ce1a
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-20 03:21:55 -0400

    Update tile utils to use 32bit colors

--- assembly/io/video/constants.ts
+++ assembly/io/video/constants.ts
@@ -2,13 +2,6 @@ export const LCD_WIDTH: u8 = 160;
 export const LCD_HEIGHT: u8 = 144;
 export const LCD_RES: u32 = LCD_WIDTH * LCD_HEIGHT;
 
-// export const PaletteColors: u8[][] = [
-//     [224, 248, 208, 255],
-//     [136, 192, 112, 255],
-//     [52, 104, 86, 255],
-//     [0, 0, 0, 255]
-// ];
-
 export const PaletteColors: u32[] = [
-    0xe0f8d0ff, 0x88c070ff, 0x346856ff, 0x000000ff
+    0xffcffde0, 0xff6fc089, 0xff566834, 0xff000000
 ];
