ef6691f65c1a8abde4b535aa7bae8e48f3a3c7aa
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-21 14:29:04 -0400

    base for handling different cartridge types

--- assembly/metadata.ts
+++ assembly/metadata.ts
@@ -1,5 +1,42 @@
 import { ByteReader } from "./utils/bytereader";
 
+export enum CartridgeType {
+    ROM_ONLY = 0x00,
+    MBC1 = 0x01,
+    MBC1_RAM = 0x02,
+    MBC1_RAM_BATTERY = 0x03,
+    MBC2 = 0x05,
+    MBC2_BATTERY = 0x06,
+    ROM_RAM_1 = 0x08,
+    ROM_RAM_BATTERY_1 = 0x09,
+    MMM01 = 0x0B,
+    MMM01_RAM = 0x0C,
+    MMM01_RAM_BATTERY = 0x0D,
+    MBC3_TIMER_BATTERY = 0x0F,
+    MBC3_TIMER_RAM_BATTERY_2 = 0x10,
+    MBC3 = 0x11,
+    MBC3_RAM_2 = 0x12,
+    MBC3_RAM_BATTERY_2 = 0x13,
+    MBC5 = 0x19,
+    MBC5_RAM = 0x1A,
+    MBC5_RAM_BATTERY = 0x1B,
+    MBC5_RUMBLE = 0x1C,
+    MBC5_RUMBLE_RAM = 0x1D,
+    MBC5_RUMBLE_RAM_BATTERY = 0x1E,
+    MBC6 = 0x20,
+    MBC7_SENSOR_RUMBLE_RAM_BATTERY = 0x22,
+    POCKET_CAMERA = 0xFC,
+    BANDAI_TAMA5 = 0xFD,
+    HuC3 = 0xFE,
+    HuC1_RAM_BATTERY = 0xFF,
+};
+
+export enum CGBMode {
+    NonCGB = 0,
+    PartialCGB = 0x80,
+    CGBOnly = 0xC0,
+}
+
 @unmanaged
 export class Metadata {
     title: string = "";
@@ -14,12 +51,15 @@ export class Metadata {
     maskRomVersionNumber: u8 = 0;
     headerChecksum: u8 = 0;
     globalChecksum: u16 = 0;
+
+    getCGBMode(): CGBMode { return (this.cgbFlag == CGBMode.CGBOnly || this.cgbFlag == CGBMode.PartialCGB) ? this.cgbFlag : CGBMode.NonCGB };
+
     static read(rom: ArrayBuffer): Metadata {
         let reader = new ByteReader(rom);
         reader.seek(0x134);
         let mt = new Metadata();
-        mt.title = reader.readString(15);
-        mt.cgbFlag = reader.readByte();
+        mt.title = reader.readString(16);
+        mt.cgbFlag = Uint8Array.wrap(rom)[0x143];
         mt.newLicenseeCode = reader.read2Bytes();
         mt.sgbFlag = reader.readByte();
         mt.cartridgeType = reader.readByte();
