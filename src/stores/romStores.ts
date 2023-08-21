4e5a71aafd3ac25cd615f275acef9997b5e7edf3
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-21 14:43:01 -0400

    fix errors on first launch with no localstorage

--- src/stores/romStores.ts
+++ src/stores/romStores.ts
@@ -2,14 +2,14 @@ import { writable, type Writable } from "svelte/store";
 import { RomType, type RomReference, type StoredRom } from "../types";
 
 const bootromKey = "bootroms";
-const storedBootRoms = JSON.parse(localStorage.getItem(bootromKey)) as StoredRom[];
+const storedBootRoms = JSON.parse(localStorage.getItem(bootromKey)) as StoredRom[] ?? [];
 export const bootRomStore: Writable<StoredRom[]> = writable<StoredRom[]>(storedBootRoms);
 bootRomStore.subscribe(value => {
     localStorage.setItem(bootromKey, JSON.stringify(value?.length > 0 ? value : []));
 });
 
 const cartRomKey = "cartroms";
-const storedcartRoms = JSON.parse(localStorage.getItem(cartRomKey)) as StoredRom[];
+const storedcartRoms = JSON.parse(localStorage.getItem(cartRomKey)) as StoredRom[] ?? [];
 
 export const cartRomStore: Writable<StoredRom[]> = writable<StoredRom[]>(storedcartRoms);
 cartRomStore.subscribe(value => {
