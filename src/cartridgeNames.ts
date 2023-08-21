0dbed34dfa29cd5dc9305c722c0035801bff6c00
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-21 19:15:19 -0400

    moves game names to public json

---
+++ src/cartridgeNames.ts
@@ -0,0 +1,23 @@
+
+let gbNames = undefined;
+let gbcNames = undefined;
+
+export async function getGbNames(): Promise<any> {
+    if (gbNames == undefined) {
+        console.log('Fetching /gbRomNames.json...');
+        const res = await fetch("/gbRomNames.json");
+        gbNames = await res.json();
+        console.log('done');
+    }
+    return gbNames;
+}
+
+export async function getGbcNames(): Promise<any> {
+    if (gbcNames == undefined) {
+        console.log('Fetching /gbcRomNames.json...');
+        const res = await fetch("/gbcRomNames.json");
+        gbcNames = await res.json();
+        console.log('done');
+    }
+    return gbcNames;
+}
