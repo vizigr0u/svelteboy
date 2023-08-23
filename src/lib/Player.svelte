2ace546b25a2c4be0140088abafad9ad5019efad
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-23 00:39:55 -0400

    improve lcd canvas view abstraction

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
     import { getGameFrame } from "../../build/release";
     import PlayerControls from "./PlayerControls.svelte";
-    import DebugLcd from "./debug/DebugLcd.svelte";
     import FpsCounter from "./debug/FPSCounter.svelte";
+    import LcdCanvas from "./LcdCanvas.svelte";
 </script>
 
-<DebugLcd draw={(buffer) => getGameFrame(buffer)} width={160} height={144} />
+<LcdCanvas
+    updateBuffer={(buffer) => getGameFrame(buffer)}
+    width={160}
+    height={144}
+/>
 <PlayerControls />
 <FpsCounter />
--- src/lib/debug/DebugSection.svelte
+++ src/lib/debug/DebugSection.svelte
@@ -4,10 +4,61 @@
     import BenchmarkControl from "./BenchmarkControl.svelte";
     import BreakpointsControl from "./BreakpointsControl.svelte";
     import CpuDebugInfo from "./CpuDebugInfo.svelte";
-    import DebugLcd from "./DebugLcd.svelte";
     import Disassembler from "./Disassembler.svelte";
     import HexDumpControl from "./HexDumpControl.svelte";
     import LogView from "./LogView.svelte";
+    import { GbDebugInfoStore } from "../../stores/debugStores";
+    import LcdCanvas from "../LcdCanvas.svelte";
+
+    let drawTiles;
+    let drawBG;
+    let pixelSize = 2;
+    let autodraw: boolean = true;
+
+    function onRefreshClick() {
+        drawTiles();
+        drawBG();
+    }
+
+    function drawBGLines(ctx: CanvasRenderingContext2D): void {
+        const minX = $GbDebugInfoStore.lcd.scX;
+        const minY = $GbDebugInfoStore.lcd.scY;
+        const maxX = (160 + minX) % ctx.canvas.width;
+        const maxY = (144 + minY) % ctx.canvas.height;
+        ctx.strokeStyle = "red";
+        ctx.beginPath();
+        if (minX < maxX) {
+            ctx.moveTo(minX, minY);
+            ctx.lineTo(maxX, minY);
+            ctx.moveTo(minX, maxY);
+            ctx.lineTo(maxX, maxY);
+        } else {
+            ctx.moveTo(0, minY);
+            ctx.lineTo(maxX, minY);
+            ctx.moveTo(minX, minY);
+            ctx.lineTo(159, minY);
+            ctx.moveTo(0, maxY);
+            ctx.lineTo(maxX, maxY);
+            ctx.moveTo(minX, maxY);
+            ctx.lineTo(159, maxY);
+        }
+        if (minY < maxY) {
+            ctx.moveTo(minX, minY);
+            ctx.lineTo(minX, maxY);
+            ctx.moveTo(maxX, minY);
+            ctx.lineTo(maxX, maxY);
+        } else {
+            ctx.moveTo(minX, 0);
+            ctx.lineTo(minX, maxY);
+            ctx.moveTo(minX, minY);
+            ctx.lineTo(minX, 143);
+            ctx.moveTo(maxX, 0);
+            ctx.lineTo(maxX, maxY);
+            ctx.moveTo(maxX, minY);
+            ctx.lineTo(maxX, 143);
+        }
+        ctx.stroke();
+    }
 </script>
 
 <div class="debug-section">
@@ -20,17 +71,32 @@
         <BenchmarkControl />
     </div>
     <div class="debug-right-panel debug-panel">
-        <DebugLcd
-            title="Tile Data"
+        <div class="canvas-controls">
+            <label>
+                auto-draw
+                <input type="checkbox" bind:checked={autodraw} />
+            </label>
+            <button on:click={onRefreshClick}>Draw now</button>
+            <input type="range" bind:value={pixelSize} min="1" max="10" />
+        </div>
+        <h4>Tile Data</h4>
+        <LcdCanvas
             width={32 * 8}
             height={16 * 8}
-            draw={(a) => drawTileData(a, 32 * 8)}
+            updateBuffer={(a) => drawTileData(a, 32 * 8)}
+            bind:draw={drawTiles}
+            bind:autodraw
+            bind:pixelSize
         />
-        <DebugLcd
-            title="BG"
+        <h4>Background</h4>
+        <LcdCanvas
             width={32 * 8}
             height={32 * 8}
-            draw={drawBackgroundMap}
+            updateBuffer={drawBackgroundMap}
+            postProcess={drawBGLines}
+            bind:draw={drawBG}
+            bind:autodraw
+            bind:pixelSize
         />
     </div>
 </div>
@@ -45,6 +111,14 @@
         flex-direction: column;
         justify-content: top;
         align-items: left;
+    }
+
+    .canvas-controls {
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        gap: 1em;
+        /* width: 35em; */
     }
 
     :global(.debug-tool-container) {
