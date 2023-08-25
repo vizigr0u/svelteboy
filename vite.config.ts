6077f85686ecc3fd15ab313961d60b48d99c7924
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-25 13:19:15 -0400

    add top level await for builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,12 @@ export default defineConfig({
     globals: {
       Buffer: true
     }
-  })],
+  }),
+  topLevelAwait({
+    // The export name of top-level await promise for each chunk module
+    promiseExportName: "__tla",
+    // The function to generate import names of top-level await promise in each chunk module
+    promiseImportName: i => `__tla_${i}`
+  }),
+  ],
 })
