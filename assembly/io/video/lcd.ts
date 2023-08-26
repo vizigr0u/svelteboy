9d48becd4253b5593ade55ebf52d408717ad6786
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-25 21:10:20 -0400

    fix dma not writing to memory

--- assembly/io/video/lcd.ts
+++ assembly/io/video/lcd.ts
@@ -5,7 +5,7 @@ import { Logger, log } from "../../debug/logger";
 import { GB_IO_START } from "../../cpu/memoryConstants";
 import { uToHex } from "../../utils/stringUtils";
 import { Ppu, PpuMode } from "./ppu";
-import { DMA_ADDRESS, Dma } from "./dma";
+import { Dma } from "./dma";
 
 const LCD_GB_START_ADDRESS: u16 = 0xFF40;
 const LCD_GBC_START_ADDRESS: u16 = 0xFF4D;
@@ -50,6 +50,12 @@ class LcdGbData {
     static getStatAddress(): u16 {
         return LCD_GB_START_ADDRESS + <u16>offsetof<LcdGbData>('stat');
     }
+
+    @inline
+    static getDmaAddress(): u16 {
+        return LCD_GB_START_ADDRESS + <u16>offsetof<LcdGbData>('dma');
+    }
+
 
     @inline
     static getLyAddress(): u16 {
@@ -120,9 +126,8 @@ export class Lcd {
     }
 
     static Store(gbAddress: u16, value: u8): void {
-        if (gbAddress == DMA_ADDRESS) {
-            // Dma.Start(value);
-            return;
+        if (gbAddress == LcdGbData.getDmaAddress()) {
+            Dma.Start(value);
         }
         if (gbAddress == LcdGbData.getLyAddress()) {
             if (Logger.verbose >= 1)
