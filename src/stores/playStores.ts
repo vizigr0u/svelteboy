976f7af98f5bd1ae14aabdb11ea4dd7742a6a725
Vladimir Nachbaur <vizigr0u@gmail.com>
2023-08-31 00:15:48 -0400

    remove unused function

--- src/stores/playStores.ts
+++ src/stores/playStores.ts
@@ -4,16 +4,3 @@ import { InputType } from "../types";
 export const GamePlaying = writable<boolean>(false);
 export const GameFrames = writable<number>(0);
 export const KeyPressMap = writable<Set<InputType>>(new Set<InputType>())
-
-function isMutuallyExclusiveKeyPressed(input: InputType, keyStore: boolean[]): boolean {
-    switch (input) {
-        case InputType.Left:
-            return keyStore[InputType.Right];
-        case InputType.Right:
-            return keyStore[InputType.Left]
-        case InputType.Up:
-            return keyStore[InputType.Down]
-        case InputType.Down:
-            return keyStore[InputType.Up]
-    }
-}
