dc7ccb8712b3efc9aa1cf25709fa72be0cd4071e
Vladimir Nachbaur <vizigr0u@gmail.com>
2026-04-10 21:49:00 -0400

    update deps and pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import topLevelAwait from "vite-plugin-top-level-await";
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -15,17 +17,11 @@ export default defineConfig({
   },
   plugins: [svelte(), nodePolyfills({
     exclude: [
-      'fs', 'process' // Excludes the polyfill for `fs` and `node:fs`.
+      'fs', 'process'
     ],
     globals: {
       Buffer: true
     }
-  }),
-  topLevelAwait({
-    // The export name of top-level await promise for each chunk module
-    promiseExportName: "__tla",
-    // The function to generate import names of top-level await promise in each chunk module
-    promiseImportName: i => `__tla_${i}`
   }),
   ],
 })
