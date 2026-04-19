92f81a9d89eff773ed7f824db37ddcf0087844ad
Vladimir Nachbaur <vizigr0u@gmail.com>
2026-04-19 13:35:58 -0400

    add scanline timing tests

--- assembly/tests/video/index.ts
+++ assembly/tests/video/index.ts
@@ -1,6 +1,8 @@
 import { testTileToRgba } from "./tileToRgbaTests";
+import { testScanlineTiming } from "./scanlineTimingTests";
 
 export function testVideo(): boolean {
     testTileToRgba();
+    testScanlineTiming();
     return true;
 }
