cdf093322b714f56d8e3d54fa4ae8ee3548f3acf
Vladimir Nachbaur <vizigr0u@gmail.com>
2026-06-10 00:08:25 -0400

    Resume also unpauses unless debug

--- src/stores/viewStore.ts
+++ src/stores/viewStore.ts
@@ -1,10 +1,19 @@
-import { writable } from "svelte/store";
-import { pauseEmulator } from "../emulator/lifecycle";
+import { get, writable } from "svelte/store";
+import { pauseEmulator, runUntilBreak } from "../emulator/lifecycle";
+import { EmulatorInitialized } from "./playStores";
+import { DebuggerAttached } from "./debugStores";
+import { shouldAutoResumeOnEnterPlay } from "./viewResume";
 
 export const playViewActive = writable<boolean>(false);
 
 export function goToPlay(): void {
     playViewActive.set(true);
+    if (shouldAutoResumeOnEnterPlay({
+        emulatorInitialized: get(EmulatorInitialized),
+        debuggerAttached: get(DebuggerAttached),
+    })) {
+        runUntilBreak();
+    }
 }
 
 export function goToHome(): void {
